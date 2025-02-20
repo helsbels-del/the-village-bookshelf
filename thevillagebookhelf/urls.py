7a5911014bd004419ea14dbb636613df382fdd20
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-20 23:54:15 +0000

    FIX: Remove baackslash in urls

--- thevillagebookhelf/urls.py
+++ thevillagebookhelf/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from share.views import my_share
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('share/', my_share, name='share'),
+    path('admin', admin.site.urls),
+    path('share', my_share, name='share'),
 ]
