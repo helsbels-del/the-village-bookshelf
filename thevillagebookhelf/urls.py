69a659141e72cc2789f87a4382f95118365bc4be
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-20 18:44:23 +0000

    FEAT: Make migrations and create test page

--- thevillagebookhelf/urls.py
+++ thevillagebookhelf/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for villagebookshelf project.
+URL configuration for thevillagebookhelf project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -20,5 +20,5 @@ from share.views import my_share
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('share/', my_share, name='share'),
+    path('share', my_share, name='share'),
 ]
--- thevillagebookhelf/wsgi.py
+++ thevillagebookhelf/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for villagebookshelf project.
+WSGI config for thevillagebookhelf project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'villagebookshelf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thevillagebookhelf.settings')
 
 application = get_wsgi_application()
