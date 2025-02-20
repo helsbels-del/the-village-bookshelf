c92c8a50e3a1f33a3f36d8a49967b80090559b68
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-20 18:58:19 +0000

    FEAT: make good for deployment

--- thevillagebookhelf/settings.py
+++ thevillagebookhelf/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0dhw@x1n6(n48s*zeqj1o28pad^v8vnhe28oe5bv@00%6+qyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
 
 
 # Application definition
