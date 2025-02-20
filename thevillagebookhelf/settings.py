66b44b6cd3672303ab0b6f3806b38bab783d0bd3
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-20 23:25:58 +0000

    FIX: Got rid of white space

--- thevillagebookhelf/settings.py
+++ thevillagebookhelf/settings.py
@@ -88,8 +88,7 @@ WSGI_APPLICATION = 'thevillagebookhelf.wsgi.application'
 #       'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 # }
-# } 
-
+# }
 DATABASES ={
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
