dd4a6962e15c8f8785489b74a2a317de96ca5223
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-22 13:54:55 +0000

    FIX: Cahnged project folder name from thevillagebookshelf to my_bookshelf and corrected name throughout project

--- my_bookshelf/urls.py
+++ my_bookshelf/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for thevillagebookhelf project.
+URL configuration for my_bookshelf project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
