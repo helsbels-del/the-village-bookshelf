c9493d722d523a1eed9608a9daee577ba9af262d
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-25 17:34:13 +0000

    FIX: Update word count for text field

--- share/models.py
+++ share/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Books(models.Model):
     # Basic details about the book
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     author = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)  
 
