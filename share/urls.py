be754345b94bf356179873f7b1b3ec1a80013816
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-02-25 21:16:59 +0000

    FIX: Move base html to correct place. Add css styling for index.html

--- share/urls.py
+++ share/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Import views from the current app
 
 urlpatterns = [
-    path('', views.home, name='share_home'),  # Homepage for "share" app
+    path('', views.home, name='home'),  # Homepage for "share" app
 ]
