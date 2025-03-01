b3acf499f1237d30343b79c191123f0e878dcd6c
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-03-01 17:23:58 +0000

    FEAT: Add login and logout redirect urls

--- my_bookshelf/settings.py
+++ my_bookshelf/settings.py
@@ -121,6 +121,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'  # Redirects to home after login
+LOGOUT_REDIRECT_URL = '/accounts/login/'  # Redirect to login after logout
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
