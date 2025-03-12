fbbe5302958883891cf78dfb43e0582e098152a3
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-03-12 14:34:34 +0000

    FEAT: Add custom creation form

--- share/forms.py
+++ share/forms.py
@@ -1,8 +1,25 @@
 from django import forms
 from .models import Books
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Books
         fields = ["title", "author", "description", "available"]
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=True)  # Add email field
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]  # Ensure email is included
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
