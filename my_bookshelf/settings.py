9e3f241398ce528fe7a544eeb6687d993e551881
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-03-14 21:42:50 +0000

    FIX: Update email detail in settings

--- my_bookshelf/settings.py
+++ my_bookshelf/settings.py
@@ -160,5 +160,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "your-email@gmail.com"
-EMAIL_HOST_PASSWORD = "your-email-password"
+EMAIL_HOST_USER = "hsmith675565@gmail.com"
+EMAIL_HOST_PASSWORD = "lhph hqsu xirm dsib"
