604b161fa15090970c44f1af7777d5fdd2f82750
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-03-15 12:34:27 +0000

    FIX: Add stylig to password html files

--- share/urls.py
+++ share/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView 
+from django.contrib.auth import views as auth_views
 from . import views
-from django.contrib.auth import auth_views
 
 
 urlpatterns = [
