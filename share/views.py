0403e351e05f43ed91134bd97dca141f1d34e20f
helsbels-del <5b64nnhi@students.codeinstitute.net>
2025-03-17 00:59:21 +0000

    FIX: Add order to list to stop warning coming up

--- share/views.py
+++ share/views.py
@@ -72,7 +72,7 @@ def search_books(request):
     
 
 def book_list(request):
-    books = Books.objects.all()
+    books = Books.objects.all().order_by('title')
     paginator = Paginator(books, 1000)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
